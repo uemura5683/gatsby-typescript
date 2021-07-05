6a310386e66f4f55bdea0432fd27ac22ba3f40ec
uemuragame5683 <nobuyoshi5683@gmail.com>
2021-07-05 22:11:45 +0900

    framer-motionの機能を追加

--- src/pages/frontend.tsx
+++ src/pages/frontend.tsx
@@ -1,13 +1,22 @@
 import * as React from "react"
 import { Link } from "gatsby"
-
 import Layout from "../components/layout"
+import { AnimatePresence, motion } from "framer-motion";
+import Seo from "../components/seo"
 
 const SecondPage = () => (
   <Layout>
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <Seo title="フロントエンド一覧" />
+    <motion.div
+      animate={{ x: 0, opacity: 1 }}
+      initial={{ x: 0, opacity: 0 }}
+      exit={{ x: 0, opacity: 0 }}
+      transition={{ duration: 0.5 }}
+    >
+      <h1>Hi from the second page</h1>
+      <p>Welcome to page 2</p>
+      <Link to="/">Go back to the homepage</Link>
+     </motion.div>
   </Layout>
 )
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,34 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
-
+import { AnimatePresence, motion } from "framer-motion";
 import Layout from "../components/layout"
+import Seo from "../components/seo"
 
 function IndexPage() {
   return (
     <Layout>
-    <div className="list">
-      <h1>Frontend</h1>
-      <StaticImage
-        src="../images/gatsby-astronaut.png"
-        width={300}
-        quality={95}
-        alt="A Gatsby astronaut"
-        style={{ marginBottom: `1.45rem` }}
-      />
-      <p>
-        <Link to="/frontend/">Go to page 2</Link> <br />
-      </p>
-    </div>
+      <Seo title="トップページ" />
+      <motion.div
+        animate={{ x: 0, opacity: 1 }}
+        initial={{ x: 0, opacity: 0 }}
+        exit={{ x: 0, opacity: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        <div className="list">
+          <h1>Frontend</h1>
+          <StaticImage
+            src="../images/gatsby-astronaut.png"
+            width={300}
+            quality={95}
+            alt="A Gatsby astronaut"
+            style={{ marginBottom: `1.45rem` }}
+          />
+          <p>
+            <Link to="/frontend/">Go to page 2</Link> <br />
+          </p>
+        </div>
+      </motion.div>
   </Layout>
   )
 }
